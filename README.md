08d1de573e7fd78e0b9eef4d5cf7a84073ab3886
hspl-rohan <rokir@heliconia.in>
2024-05-01 10:40:00 +0530

    project init with shadcn ui

--- README.md
+++
@@ -1,2 +0,0 @@
-# tools
-curated list of resources and tools for developers, designers and indie hackers.
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <main>
+        <h1>Welcome to Tools by @rohan-kiratsata</h1>
+      </main>
+    </>
+  );
+}
